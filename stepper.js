32df3c716be2f411ec8c33edc4492d26abf6db0c
Ronald Brinkerink <r.brinkerink@brein.nl>
2016-12-28 18:36:29 +0100

    Update stepper.js

--- stepper.js
+++ stepper.js
@@ -4,9 +4,11 @@ module.exports = function(RED) {
     function Stepper(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+        this.log('init stepper');
+        this.log('StepperWiringPi ' + (StepperWiringPi ? true : false));
         var motor = StepperWiringPi.setup(config.steps, config.pin1, config.pin2);
+        this.log('motor initialized ' + (motor ? true : false));
         this.on('input', function(msg) {
-            msg.payload  = motor ? true : false;
             node.send(msg);
         });
     }
